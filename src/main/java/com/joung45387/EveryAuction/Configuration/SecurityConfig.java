b68a90a0bb41dad568db3c5bc66cb14c2e0b6a67
joung45387 <joung45387@gmail.com>
2023-01-14 20:57:31 +0900

    update

--- src/main/java/com/joung45387/EveryAuction/Configuration/SecurityConfig.java
+++ src/main/java/com/joung45387/EveryAuction/Configuration/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         http.headers().frameOptions().sameOrigin();
         http
                 .authorizeHttpRequests()
-                .antMatchers("/", "/signin", "/signup", "/css/**", "/item/**", "/js/**").permitAll()
+                .antMatchers("/", "/signin", "/signup", "/css/**", "/item/**", "/js/**", "/fragments/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
