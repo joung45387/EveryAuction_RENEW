1125f5eda962fab6d28c88ed16c7d07bd9ba2d16
joung45387 <joung45387@gmail.com>
2023-01-16 20:11:30 +0900

    update

---
+++ delay.sh
@@ -0,0 +1,17 @@
+echo "> 서버 체크 시작"
+          for retry_count in {1..10};
+          do
+            response=$(sudo curl -s http://54.180.119.91:8080/actuator/health)
+            up_count=$(echo $response | grep 'UP' | wc -l)
+            echo "> $retry_count : $response  : $up_count"
+            if [ $up_count -ge 1 ]; then
+              echo "> 서버 health 체크 성공"
+              break
+            fi
+            if [ $retry_count -eq 10 ]; then
+              echo "> 서버 health 체크 실패"
+              exit 1
+            fi
+            echo "> 실패 10초후 재시도"
+            sleep 10
+          done
